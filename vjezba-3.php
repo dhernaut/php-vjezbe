568f60c7e9ab59683e8ef4efdaa3c446515c9ecc
dhernaut <denis.hernaut@fer.hr>
2025-10-24 12:42:18 +0200

    Add files via upload

---
+++ vjezba-3.php
@@ -0,0 +1,21 @@
+<?php
+$title = "Da Vincijev kod";
+$link = "https://hr.wikipedia.org/wiki/Da_Vincijev_kod";
+
+echo '
+<!DOCTYPE html>
+<html lang="hr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>' . $title . '</title>
+</head>
+<body>
+  <h1>' . $title . '</h1>
+  <p>' . $title . ' je kriminalistički triler američkog pisca Dana Browna.</p>
+  <a href="' . $link . '" target="_blank">Poveznica</a>
+</body>
+</html>
+<!-- vjezba-3.php -->
+';
+?>
